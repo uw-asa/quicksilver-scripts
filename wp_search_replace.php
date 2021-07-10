7d937124c8cbe059afc19d293302c564645d14fd
Bradley Bell <bradley.bell@gmail.com>
2021-07-09 22:33:44 -0700

    use getenv

--- wp_search_replace.php
+++ wp_search_replace.php
@@ -1,21 +1,25 @@
 <?php
 echo "Replacing previous environment urls with new environment urls... \n";
 
-define('DRY_RUN', '');
-// define('DRY_RUN', ' --dry-run');
+if (getenv('DRY_RUN')) {
+    define('DRY_RUN', ' --dry-run');
+} else {
+    define('DRY_RUN', '');
+}
 
 // run this remotely with something like:
 // WPCLI="terminus remote:wp cms1-asa-uw.dev --" PANTHEON_SITE_NAME=cms1-asa-uw PANTHEON_ENVIRONMENT=dev php code/private/scripts/wp_search_replace.php
-if (array_key_exists('WPCLI', $_ENV)) { 
-    define('WPCLI', $_ENV['WPCLI']);
+if ($val = getenv('WPCLI')) {
+    define('WPCLI', $val);
 } else {
     define('WPCLI', 'wp');
 }
 
-if ( ! empty( $_ENV['PANTHEON_ENVIRONMENT'] ) ) {
-    $asa_domain = preg_replace('/(cms\d+)-asa-uw/', '$1.asa.uw.edu', $_ENV['PANTHEON_SITE_NAME']);
+if ( $env = getenv('PANTHEON_ENVIRONMENT') ) {
+    $asa_domain = preg_replace('/(cms\d+)-asa-uw/', '$1.asa.uw.edu', getenv('PANTHEON_SITE_NAME'));
+    echo "Site is {$asa_domain}\n";
 
-    switch( $_ENV['PANTHEON_ENVIRONMENT'] ) {
+    switch( $env ) {
     default:
         echo "Unknown environment. Not doing anything\n";
         break;
@@ -50,11 +54,11 @@ if ( ! empty( $_ENV['PANTHEON_ENVIRONMENT'] ) ) {
             // dev or test site, <site>.[dev|test].cms[1|2].asa.uw.edu
             // or pending live site, <site>.cms[1|2].asa.uw.edu
             } elseif (preg_match('/^([^\.\s]+)\.(?:dev\.|test\.|)cms\d+\.asa\.uw\.edu$/', $site['domain'], $matches)) {
-                $site['newdomain'] = "{$matches[1]}.{$_ENV['PANTHEON_ENVIRONMENT']}.{$asa_domain}";
+                $site['newdomain'] = "{$matches[1]}.{$env}.{$asa_domain}";
 
             // live site, <site>.uw.edu or <site>.washington.edu
             } elseif (preg_match('/^(?:dev\.)?([^\.\s]+)\.(?:uw|washington)\.edu$/', $site['domain'], $matches)) {
-                $site['newdomain'] = "{$matches[1]}.{$_ENV['PANTHEON_ENVIRONMENT']}.{$asa_domain}";
+                $site['newdomain'] = "{$matches[1]}.{$env}.{$asa_domain}";
 
             } else {
                 echo "Don't know how to translate site {$site['domain']}, skipping\n";
